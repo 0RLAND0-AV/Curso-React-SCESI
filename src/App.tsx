7318187a026ddf0dc08b94f04434635ebf01eae0
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-10-05 13:24:49 -0400

    feat: Agregando contador y input haciendo uso del hook usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 import './App.css'
 import NombreCurso from './components/Nombre'
 import PresentacionOAV from './src_orlando_av/PresentacionOAV'
-
+import { useState } from 'react';
 
 function App() {
+  let [X,setX] = useState<number>(0);
+  let [contadorState, setContadorState] = useState<number>(0);
+  function restarContador() {
+    setContadorState(contadorState - 1);
+  }
   return (
     <>
       <h1>
         <NombreCurso /> 
       </h1>
+      
+      <PresentacionOAV nombre="OrlandoAV" descripcion="Uso de Props con atributo edad no obligatorio, valor por defecto" />
+      
       <div>
-        <PresentacionOAV nombre="OrlandoAV" descripcion="Uso de Props con valor edad no obligatorio, valor por defecto" />
+        <h2>Contador: {contadorState}</h2>
+        <button onClick={() => {
+                                setContadorState(contadorState + 1);
+                                // console.log("Contador: ", contadorState + 1);
+                              }}>
+          Incrementar Contador usando una funcion flecha
+        </button>
+        <button onClick={restarContador}>
+          Decrementar Contador usando una funcion normal
+        </button>
       </div>
+      <div>
+        <h2>Valor X renderizado en tiempo real: {X}</h2>
+      </div>
+      <input type="Number" placeholder='Ingrese nuevo valor X' onChange={(e) => setX(Number(e.target.value))} />
+
     </>
   )
 }
-
 
 export default App
