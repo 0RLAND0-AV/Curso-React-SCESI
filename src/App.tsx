9635eb22b00e6858437f8f55777972f1beb78635
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-10-05 12:26:53 -0400

    feat: Uso de Props con aributo no obligatorio y valor por defecto, del componente PresentacionOAV.tsx en main.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NombreCurso from './components/Nombre'
+import PresentacionOAV from './src_orlando_av/PresentacionOAV'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1>
-      ORLANDO ALTAMIRNO VARGAS
-    </h1>
+      <h1>
+        <NombreCurso /> 
+      </h1>
+      <div>
+        <PresentacionOAV nombre="OrlandoAV" descripcion="Uso de Props con valor edad no obligatorio, valor por defecto" />
+      </div>
     </>
   )
 }
+
 
 export default App
---
+++ src/components/Nombre.tsx
@@ -0,0 +1,6 @@
+function Nombre() { 
+    return <>Curso React con Ts</> 
+    // return <h1>Orlando</h1> 
+}
+
+export default Nombre;
