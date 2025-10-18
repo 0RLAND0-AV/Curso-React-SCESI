903b646c7aada2a1b5f836c09bd898c41551b359
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-10-18 18:52:34 -0400

    test actualizar deploy

--- src/src_orlando_av/PresentacionOAV.tsx
+++ src/src_orlando_av/PresentacionOAV.tsx
@@ -8,6 +8,7 @@ interface PropsOAV{
 const PresentacionOAV = ({nombre, edad=0, descripcion}: PropsOAV) => { 
     return (
         <>
+        <h1>PRUEBA </h1>
             <h2>Nombre: {nombre}</h2>
             <p>Descripcion: {descripcion}</p>
             <p>Edad: {edad}</p>
